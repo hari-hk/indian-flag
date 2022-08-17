e93615f97bc866d26f186d9e78f93541f4d6cd36
hari hara kumar <hariharakumar5196@gmail.com>
2022-08-17 23:30:47 +0530

    refactor code

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const wheel = document.getElementById('wheel');
-
-const spikes = new Array(12).fill('').map((el, idx) => idx + 1);
-
-const spike = spikes.map((spike, idx) => {
+const spikes = new Array(12).fill('').map((e, idx) => idx + 1);
+const spike = spikes.map((spike) => {
   const lines = document.createElement('div');
-  lines.className = 'line';
   const transform = `rotate(${spike * 15}deg)`;
+  lines.className = 'line';
   lines.style.transform = transform;
   return lines;
 });
